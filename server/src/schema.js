21f12450d00a5520b959477ca48f4ce6534c5e00
Babatunde Sanusi <swisskid95@gmail.com>
2020-03-15 13:54:15 +0100

    setup data graph's schema

---
+++ server/src/schema.js
@@ -0,0 +1,55 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  # Schema goes here
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String!): String #Login Token
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`;
+
+module.exports = typeDefs;
